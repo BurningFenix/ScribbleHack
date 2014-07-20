3faf071b6a9e8ebd50abdc448922a38d8252a65a
itslikeroar <rochecodeperu@hotmail.com>
2014-07-19 23:36:10 -0400

    ro changes

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import *
 
 urlpatterns = patterns('apps.accounts.views',
-    url(r'^profile/', 'profile', name="accounts_profile"),
+    url(r'^profile/$', 'profile', name="accounts_profile"),
     url(r'^profile/edit', 'edit_profile', name="accounts_edit"),
     #url(r'^browse/$', 'browse', name="homepage_browse"),
     #url(r'^confirmed/$', 'congrats', name="homepage_congrats"),
--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login
 
 def index(request): #Homepage
     return render_to_response('homepage/homepage.html', context_instance=RequestContext(request))
 
-def login(request): #Login
-    return render_to_response('homepage/login.html', context_instance=RequestContext(request))
+def login_user(request): #Login
+	if(len(request.POST) > 0):
+		user = request.POST['username']
+		pw = request.POST['password']
+		user = authenticate(username=user, password=pw)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return render_to_response('profile.html', context_instance=RequestContext(request))
+			else:
+				print("user not active")
+		else:
+			print("user is none")
+	return render_to_response('homepage/login.html', context_instance=RequestContext(request))
 
 #def browse(request): #Forgot Password Page
     #return render_to_response('homepage/browse.html', context_instance=RequestContext(request))
