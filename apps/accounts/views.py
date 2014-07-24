b6a890ce6bfa2aca4c0f697e4e01694d1ec08e0e
itslikeroar <rochecodeperu@hotmail.com>
2014-07-24 15:36:44 -0400

    user information is now retrieved from the html templates instead of setting each variable in the views and passing them to the template

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -6,14 +6,8 @@ from apps.accounts.forms import EditProfileForm
 
 @login_required(login_url='/accounts/login/')
 def profile_view(request): #Profile Page
-	age = request.user.age
-	favorite_book = request.user.favorite_book
-	favorite_hero = request.user.favorite_hero
-	return render_to_response(
-		'accounts/profile.html',
-		{'age':age, 'favorite_book':favorite_book, 'favorite_hero':favorite_hero},
-		context_instance=RequestContext(request)
-	)
+	return render_to_response('accounts/profile.html',
+		context_instance=RequestContext(request))
 
 @login_required(login_url='/accounts/login/')
 def edit_profile_view(request): #Edit Profile Pages
@@ -25,13 +19,17 @@ def edit_profile_view(request): #Edit Profile Pages
 			request.user.favorite_book = form.cleaned_data['favorite_book']
 			request.user.favorite_hero = form.cleaned_data['favorite_hero']
 			request.user.save()
+			return render_to_response('accounts/profile.html',
+				context_instance=RequestContext(request))
 	else:
 		form = EditProfileForm()
-	return render_to_response('accounts/edit_profile.html', {'form': form}, context_instance=RequestContext(request))
+	return render_to_response('accounts/edit_profile.html', {'form': form},
+		context_instance=RequestContext(request))
 
 def logout_view(request):
 	# attepmts to log user out only if the user was already logged in
 	if request.user.is_authenticated():
 		logout(request)
 	# probably should add something to show that the user was logged out successfully
-	return render_to_response('homepage/homepage.html', context_instance=RequestContext(request))
+	return render_to_response('homepage/homepage.html',
+		context_instance=RequestContext(request))
