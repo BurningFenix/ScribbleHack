de59cbbbd835492afd9c08cad111f11d6d8247e6
itslikeroar <rochecodeperu@hotmail.com>
2014-07-26 12:00:51 -0400

    extended register view form validation to remove checking for things in register_view

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 class EditProfileForm(forms.Form):
 	age = forms.IntegerField(required=False)
@@ -8,7 +9,17 @@ class EditProfileForm(forms.Form):
 		max_length=20, required=False)
 
 class RegisterForm(forms.Form):
-	username = forms.CharField(max_length=30)
-	password = forms.CharField(widget=forms.PasswordInput)
-	checkPassword = forms.CharField(label='Type your password again',
-		widget=forms.PasswordInput)
+	username = forms.CharField(max_length=30, required=True)
+	password = forms.CharField(widget=forms.PasswordInput, required=True)
+	password2 = forms.CharField(label='Type your password again',
+		widget=forms.PasswordInput, required=True)
+
+	def clean(self):
+		cleaned_data = super(RegisterForm, self).clean()
+		pw1 = cleaned_data.get('password')
+		pw2 = cleaned_data.get('password2')
+
+		if pw1 and pw2 and pw1 != pw2:
+			raise forms.ValidationError(
+				_('Passwords are not the same'), code='Unequal passwords')
+		return cleaned_data
