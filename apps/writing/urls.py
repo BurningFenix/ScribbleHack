5930ae96689865a3ef76ce5a72e7ccb2b7b49d5e
itslikeroar <rochecodeperu@hotmail.com>
2014-08-05 21:57:00 -0400

    writing view now displays a list of links to all the writing pieces on the database

--- apps/writing/urls.py
+++ apps/writing/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
-from .views import WritingListView, CreateWritingView
+from .views import WritingListView, CreateWritingView, WritingDetailView
 
 urlpatterns = patterns('',
 	url(r'^$', WritingListView.as_view()),
-	url(r'^create', CreateWritingView.as_view(),
+	url(r'^create/$', CreateWritingView.as_view(),
 		name='create_writing'),
+	url(r'^(?P<pk>\d+)/$', WritingDetailView.as_view(),
+		name='writing_detail'),
 )
