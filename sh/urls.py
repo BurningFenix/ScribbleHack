18b321361aeed83943fd9c7541c33a6586a8af58
itslikeroar <rochecodeperu@hotmail.com>
2014-08-07 13:56:39 -0400

    moved homepage into sh project directory also made the views in the homepage class based. much easier to read.

--- sh/urls.py
+++ sh/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
-
+from .views import IndexView, ArtBrowseView, WritingBrowseView
 from django.contrib import admin
 admin.autodiscover()
 
 handler404 = 'sh.views.custom404'
 
 urlpatterns = patterns('',
-    (r'^', include('apps.homepage.urls')),
-    (r'^accounts/', include('apps.accounts.urls')),
+    url(r'^$', IndexView.as_view()),
+    url(r'^art$', ArtBrowseView.as_view()),
+    url(r'^writing$', WritingBrowseView.as_view()),
+    url(r'^accounts/', include('apps.accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'universe/', include('apps.universe.urls')),
     url(r'writing/', include('apps.writing.urls')),
--- sh/views.py
+++ sh/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import TemplateView
 import random
 
+# Homepage
+class IndexView(TemplateView):
+	template_name = 'homepage/homepage.html'
+
+# Both of these will be removed in the future since art and writing
+# will become their own apps
+############
+class ArtBrowseView(TemplateView):
+    template_name = 'homepage/art_browse.html'
+
+class WritingBrowseView(TemplateView):
+    template_name = 'homepage/writing_browse.html'
+############
+
+# Picks a random 404 page
 def custom404(request):
 	template = '404error1.html'
 	if random.random() > .5:
